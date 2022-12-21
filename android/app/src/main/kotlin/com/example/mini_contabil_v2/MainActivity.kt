fc5afa64a9397c4014227ba30e539e1189ed4be9
adilsonchameia <adilsonkchameia@outlook.com>
2022-12-21 13:19:51 +0100

    Add existing project files to Git

---
+++ android/app/src/main/kotlin/com/example/mini_contabil_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mini_contabil_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mini_contabil_v2/interest.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
